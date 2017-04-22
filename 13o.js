751c181084ac2d855cd0da4451191609a6b20f46
Selectful <jshao@csupomona.edu>
2017-04-21 18:13:02 -0700

    Update 13o.js

--- 13o.js
+++ 13o.js
@@ -1,16 +1,13 @@
 var betterTotal = 0;
 var dealerTotal = 0;
-var buston10 = 0;
-
 
 function getRandomArbitrary() {
 	var betterSum = 0;
 	var dealerSum = 0;
-	while (betterSum < 10) {
+	while (betterSum <= 10) {
   		betterSum += Math.floor(Math.random() * 6) + 1;
 	}
 	if (betterSum > 13) {
-		buston10 += 1;
 		dealerTotal += 1;
 	}
 	else {
@@ -24,18 +21,13 @@ function getRandomArbitrary() {
 			getRandomArbitrary();
 		}
 		else {
-			console.log("dealerSum: " + dealerSum + " betterSum: " + betterSum)
 			betterTotal += 1;
 		}
 	}
 }
 
-for (var i = 0; i < 50000; i++) {
-console.log(getRandomArbitrary());
-console.log("betterTotal:" + betterTotal + " dealerTotal:" + dealerTotal + " buston10:" + buston10)
+
+for (var i = 0; i < 100000; i++) {
+getRandomArbitrary();
+if (i === 99999) console.log((betterTotal / 1000) + "%");
 }
-
-
-
-
-
